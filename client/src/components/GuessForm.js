b6364563543a63107b414842cdc6454c1fe52609
Sam Shulman <sshulman39@gmail.com>
2023-05-18 10:48:29 -0400

    some stylin for clue tile

--- client/src/components/GuessForm.js
+++ client/src/components/GuessForm.js
@@ -6,38 +6,34 @@ const GuessForm = (props) => {
   const [isAnswerCorrect, setIsAnswerCorrect] = useState(null);
   const history = useHistory();
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-
-    // Compare user's answer with the correct answer
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
     const isCorrect = userAnswer === props.correctAnswer;
     setIsAnswerCorrect(isCorrect);
   };
 
   const handleNextQuestion = () => {
-    // Reset form and answer status
     setUserAnswer("");
     setIsAnswerCorrect(null);
-
-    // Redirect back to GameBoard
     history.push("/game");
   };
 
   return (
-    <div>
+    <div className="guess-form">
       <form onSubmit={handleFormSubmit}>
         <input
           type="text"
           value={userAnswer}
-          onChange={(e) => setUserAnswer(e.target.value)}
+          onChange={(event) => setUserAnswer(event.target.value)}
+          className="guess-input"
         />
-        <button type="submit">Submit</button>
+        <button type="submit" className="submit-button">Submit</button>
       </form>
 
       {isAnswerCorrect !== null && (
-        <div>
-          {isAnswerCorrect ? <p>Correct answer!</p> : <p>Incorrect answer!</p>}
-          <button onClick={handleNextQuestion}>Next Question</button>
+        <div className="answer-status">
+          {isAnswerCorrect ? <p className="correct-answer">Correct answer!</p> : <p className="incorrect-answer">Incorrect answer!</p>}
+          <button onClick={handleNextQuestion} className="next-button">Next Question</button>
         </div>
       )}
     </div>
