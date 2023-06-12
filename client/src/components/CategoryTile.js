cb5d4e33f3fa4dc5605b6ee4e2e8a397975c609a
Sam Shulman <sshulman39@gmail.com>
2023-06-12 13:16:41 -0400

    styled a little

--- client/src/components/CategoryTile.js
+++ client/src/components/CategoryTile.js
@@ -1,127 +1,124 @@
-import React, { useState, useEffect } from "react";
-import Modal from "react-modal";
+import React, { useState, useEffect } from "react"
+import Modal from "react-modal"
 
 const CategoryTile = (props) => {
-  const { gameId, name, questions, addScore, score } = props;
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(null);
-  const [guess, setGuess] = useState("");
-  const [showModal, setShowModal] = useState(false);
-  const [isAnswerCorrect, setIsAnswerCorrect] = useState([]);
-  const [isGuessEmpty, setIsGuessEmpty] = useState(false);
- 
+  const { gameId, name, questions, addScore, score } = props
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(null)
+  const [guess, setGuess] = useState("")
+  const [showModal, setShowModal] = useState(false)
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState([])
+  const [isGuessEmpty, setIsGuessEmpty] = useState(false)
+
   useEffect(() => {
-    Modal.setAppElement("body");
-  }, []);
+    Modal.setAppElement("body")
+  }, [])
 
   const calculateLevenshteinDistance = (str1, str2) => {
-    const m = str1.length;
-    const n = str2.length;
-  
-    if (m === 0) return n;
-    if (n === 0) return m;
-  
-    const d = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
-  
+    const m = str1.length
+    const n = str2.length
+
+    if (m === 0) return n
+    if (n === 0) return m
+
+    const d = Array.from(Array(m + 1), () => Array(n + 1).fill(0))
+
     for (let i = 0; i <= m; i++) {
-      d[i][0] = i;
+      d[i][0] = i
     }
-  
+
     for (let j = 0; j <= n; j++) {
-      d[0][j] = j;
+      d[0][j] = j
     }
-  
+
     for (let j = 1; j <= n; j++) {
       for (let i = 1; i <= m; i++) {
-        const cost = str1[i - 1] === str2[j - 1] ? 0 : 1;
+        const cost = str1[i - 1] === str2[j - 1] ? 0 : 1
         d[i][j] = Math.min(
           d[i - 1][j] + 1,
           d[i][j - 1] + 1,
           d[i - 1][j - 1] + cost
-        );
+        )
       }
     }
-  
-    return d[m][n];
-  };
+
+    return d[m][n]
+  }
   const levenshteinThreshold = 1
 
   const removeHtmlTags = (text) => {
-    return text.replace(/<\/?[^>]+(>|$)/g, "").replace(/[\\"]/g, "");
-  };
+    return text.replace(/<\/?[^>]+(>|$)/g, "").replace(/[\\"]/g, "")
+  }
 
   const removeNonTextSymbols = (text) => {
-    const withoutHtmlTags = text.replace(/<\/?[^>]+(>|$)/g, "");
-  
-    const withoutQuotes = withoutHtmlTags.replace(/['"]/g, "");
-  
-    const withoutApostrophes = withoutQuotes.replace(/'/g, "");
-  
-    const withoutDashes = withoutApostrophes.replace(/-/g, " ");
-  
-    const withoutNonTextSymbols = withoutDashes.replace(/[^\w\s]/g, "");
-  
-    const withoutArticles = withoutNonTextSymbols.replace(/\b(a|the)\b/gi, "");
-  
-    return withoutArticles.trim();
-  };
-  
+    const withoutHtmlTags = text.replace(/<\/?[^>]+(>|$)/g, "")
+
+    const withoutQuotes = withoutHtmlTags.replace(/['"]/g, "")
+
+    const withoutApostrophes = withoutQuotes.replace(/'/g, "")
+
+    const withoutDashes = withoutApostrophes.replace(/-/g, " ")
+
+    const withoutNonTextSymbols = withoutDashes.replace(/[^\w\s]/g, "")
+
+    const withoutArticles = withoutNonTextSymbols.replace(/\b(a|the)\b/gi, "")
+
+    return withoutArticles.trim()
+  }
+
   const removeParentheses = (text) => {
-    return text.replace(/\([^)]*\)/g, "");
-  };
+    return text.replace(/\([^)]*\)/g, "")
+  }
 
   const handleQuestionClick = (index) => {
-  
-      setCurrentQuestionIndex(index);
-      setShowModal(true);
-  
-  };
-  
+    setCurrentQuestionIndex(index)
+    setShowModal(true)
+  }
+
   const handleSubmit = (event) => {
-    event.preventDefault();
-  
-    const question = questions[currentQuestionIndex];
-    const answer = removeNonTextSymbols(question.answer.toLowerCase().trim());
-    const guessFormatted = guess.toLowerCase().trim();
-    const arrayOfCorrectAnswers = answer.split(" ");
+    event.preventDefault()
+
+    const question = questions[currentQuestionIndex]
+    const answer = removeNonTextSymbols(question.answer.toLowerCase().trim())
+    const guessFormatted = guess.toLowerCase().trim()
+    const arrayOfCorrectAnswers = answer.split(" ")
     const isCorrect =
       removeNonTextSymbols(guessFormatted) === answer ||
       arrayOfCorrectAnswers.includes(guessFormatted) ||
       guess.toLowerCase() === removeHtmlTags(question.answer).toLowerCase() ||
       guessFormatted === removeParentheses(answer) ||
-      calculateLevenshteinDistance(guessFormatted, answer) <= levenshteinThreshold;
-  
+      calculateLevenshteinDistance(guessFormatted, answer) <= levenshteinThreshold
+
     if (guessFormatted === "") {
-      setIsGuessEmpty(true);
-      return;
+      setIsGuessEmpty(true)
+      return
     }
-  
-    setIsGuessEmpty(false);
+
+    setIsGuessEmpty(false)
     setIsAnswerCorrect((prevAnswers) => {
-      const updatedAnswers = [...prevAnswers];
-      updatedAnswers[currentQuestionIndex] = isCorrect;
-  
+      const updatedAnswers = [...prevAnswers]
+      updatedAnswers[currentQuestionIndex] = isCorrect
+
       if (isCorrect) {
-        addScore(question.value);
+        addScore(question.value)
       }
-  
-      return updatedAnswers;
-    });
-    setGuess("");
+
+      return updatedAnswers
+    })
+    setGuess("")
     setTimeout(() => {
-      setShowModal(false);
-    }, 2000);
-    props.onQuestionAnswered(); 
-  };
-  
-    
+      setShowModal(false)
+    }, 2000)
+    props.onQuestionAnswered()
+  }
+
   const renderQuestionForm = () => {
-    const question = questions[currentQuestionIndex];
-    const answerCorrect = isAnswerCorrect[currentQuestionIndex];
-  
+    const question = questions[currentQuestionIndex]
+    const answerCorrect = isAnswerCorrect[currentQuestionIndex]
+
     if (question) {
-      const answer = removeHtmlTags(question.answer);
-  
-    return (
+      const answer = removeHtmlTags(question.answer)
+
+      return (
         <div className="guess-form-container">
           <form className="guess-form" onSubmit={handleSubmit}>
             <p className="question-text">{question.question}</p>
@@ -136,40 +133,44 @@ const CategoryTile = (props) => {
               <p className="error-message">Please enter your guess.</p>
             )}
             {answerCorrect !== undefined && (
-              <p className={`feedback-text ${answerCorrect ? "correct" : "incorrect"}`}>
-                {answerCorrect ? "Correct!" : `Incorrect. The correct answer is ${answer}.`}
+              <p
+                className={`feedback-text ${
+                  answerCorrect ? "correct" : "incorrect"
+                }`}
+              >
+                {answerCorrect
+                  ? "Correct!"
+                  : `Incorrect. The correct answer is ${answer}.`}
               </p>
             )}
           </form>
         </div>
-      );
+      )
     }
-    return null;
-  };
-  
-  
+    return null
+  }
 
   const renderQuestionBlocks = () => {
     return questions.map((question, index) => {
-      const answerCorrect = isAnswerCorrect[index];
-      const isAnswered = answerCorrect !== undefined;
-    return (
+      const answerCorrect = isAnswerCorrect[index]
+      const isAnswered = answerCorrect !== undefined
+      return (
         <div
           key={question.id}
-          className={`question-block ${isAnswered ? 'answered' : ''}`}
+          className={`question-block ${isAnswered ? "answered" : ""}`}
           onClick={!isAnswered ? () => handleQuestionClick(index) : null}
         >
-          <p className="question-text">{isAnswered ? '' : question.value}</p>
+          <p className="question-text">{isAnswered ? "" : question.value}</p>
         </div>
-      );
-    });
-  };
+      )
+    })
+  }
   const capitalizeEachWord = (str) => {
     return str
       .split(" ")
       .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(" ");
-  };
+      .join(" ")
+  }
 
   return (
     <div className="category-tile cell small-2">
@@ -183,9 +184,7 @@ const CategoryTile = (props) => {
         <div className="modal-header">
           <h2 className="modal-title">Question</h2>
         </div>
-        <div className="modal-body">
-          {renderQuestionForm()}
-        </div>
+        <div className="modal-body">{renderQuestionForm()}</div>
         <div className="modal-footer">
           <button className="modal-button" onClick={() => setShowModal(false)}>
             Cancel
@@ -196,8 +195,7 @@ const CategoryTile = (props) => {
         </div>
       </Modal>
     </div>
+  )
+}
 
-  );
-};
-
-export default CategoryTile;
+export default CategoryTile
--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,50 +1,50 @@
-import React, { useEffect, useState } from "react";
-import { Link, Redirect } from "react-router-dom";
-import getCurrentUser from "../services/getCurrentUser";
+import React, { useEffect, useState } from "react"
+import { Link, Redirect } from "react-router-dom"
+import getCurrentUser from "../services/getCurrentUser"
 
 const HomePage = (props) => {
   const [game, setGame] = useState({
     id: "",
     score: "",
-    userId: "",
-  });
-  const [redirect, setRedirect] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+    userId: ""
+  })
+  const [redirect, setRedirect] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
   useEffect(() => {
     const checkUserLogin = async () => {
       try {
-        const user = await getCurrentUser();
-        const userLoggedIn = user !== null;
-        const userIdExists = !!user?.id;
-        setIsLoggedIn(userLoggedIn && userIdExists);
+        const user = await getCurrentUser()
+        const userLoggedIn = user !== null
+        const userIdExists = !!user?.id
+        setIsLoggedIn(userLoggedIn && userIdExists)
       } catch (err) {
-        console.log(`Error in fetch: ${err.message}`);
-        setIsLoggedIn(false);
+        console.log(`Error in fetch: ${err.message}`)
+        setIsLoggedIn(false)
       }
-    };
+    }
 
-    checkUserLogin();
-  }, []);
+    checkUserLogin()
+  }, [])
 
   const getGame = async () => {
     try {
-      const response = await fetch(`/api/v1/games`);
+      const response = await fetch(`/api/v1/games`)
       if (!response.ok) {
-        const errorMessage = `${response.status} (${response.statusText})`;
-        const error = new Error(errorMessage);
-        throw error;
+        const errorMessage = `${response.status} (${response.statusText})`
+        const error = new Error(errorMessage)
+        throw error
       }
-      const data = await response.json();
-      setGame(data.game);
-      setRedirect(true);
+      const data = await response.json()
+      setGame(data.game)
+      setRedirect(true)
     } catch (err) {
-      console.log(`Error in fetch: ${err.message}`);
+      console.log(`Error in fetch: ${err.message}`)
     }
-  };
+  }
 
   if (redirect) {
-    return <Redirect to={`games/${game.id}`} />;
+    return <Redirect to={`games/${game.id}`} />
   }
 
   return (
@@ -59,7 +59,7 @@ const HomePage = (props) => {
         <p>You must be signed in to play a game.</p>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
