ab603c5242c194d4484d09d4b797acc40acdbf5a
Sam Shulman <sshulman39@gmail.com>
2023-06-15 16:09:22 -0400

    storing categories and clues

--- server/src/serializers/GameSerializer.js
+++ server/src/serializers/GameSerializer.js
@@ -1,4 +1,6 @@
 import JServiceClient from "../apiClient/jService.js"
+import Category from "../models/Category.js"
+import Clue from "../models/Clue.js"
 
 class GameSerializer {
     static async getSummary(game) {
@@ -8,6 +10,12 @@ class GameSerializer {
             serializedGame[attribute] = game[attribute]
         }
        const categories = await JServiceClient.getCategories()
+       categories.forEach(async (category) => {
+       const newCategory = await Category.query().insertAndFetch({name: category.category})
+        category.clues.forEach(async(clue) => {
+            await Clue.query().insert({value: clue.value, categoryId: newCategory.id, question: clue.question, answer: clue.answer })
+        })
+       })
        serializedGame.categories = categories
         return serializedGame
     }
